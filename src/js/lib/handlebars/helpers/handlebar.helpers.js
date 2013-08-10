3484aa444070c4f4410ced4bb4b02bbc7b17da7d
Ben Hughes <benhuggy@gmail.com>
2013-08-11 00:31:58 +0100

    Switched over to bootstrap styling

--- src/js/lib/handlebars/helpers/handlebar.helpers.js
+++ src/js/lib/handlebars/helpers/handlebar.helpers.js
@@ -3,9 +3,9 @@ Handlebars.registerHelper('customBuilder', function (data, i) {
     var html = "";
     switch (data.type) {
     case 'text':
-        html = "<li><label>" + Handlebars.Utils.escapeExpression(data.label) + "</label>" +
-            "<input data-customid=\"" + data.customid +  "\" class=\"custom customText\" type=\"text\"/>" +
-            "<span class=\"help\">" + Handlebars.Utils.escapeExpression(data.help) + "</span></li>";
+        html = "<li>" +
+            "<input data-customid=\"" + data.customid +  "\" class=\"custom customText form-control\" placeholder=\"" + Handlebars.Utils.escapeExpression(data.label) + "\" type=\"text\"/>" +
+            "<span class=\"help-block\">" + Handlebars.Utils.escapeExpression(data.help) + "</span></li>";
         break;
     default :
         break;
