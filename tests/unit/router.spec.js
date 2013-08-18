8bf58898fe6b9d3853323963368e66b6f0512352
Ben Hughes <benhuggy@gmail.com>
2013-08-18 21:50:29 +0100

    !00% test coverage for router

--- tests/unit/router.spec.js
+++ tests/unit/router.spec.js
@@ -1,8 +1,11 @@
+//overwrite the log. We don't want console.log to be fired
 define('log', function () {
     return jasmine.createSpy('log');
 });
 define(['router'], function (router) {
+    var routerFunctions;
     beforeEach(function () {
+        routerFunctions = router.prototype;
 
 
     });
@@ -18,6 +21,45 @@ define(['router'], function (router) {
                 expect(Backbone.history.start).toHaveBeenCalled();
             })
         })
+        describe('main', function () {
+            it('should call require with view.main and a function', function () {
+                spyOn(window, 'require');
+                routerFunctions.main();
+                var requreFunction = window.require.argsForCall[0][1];
+                var testFunc = jasmine.createSpy('testFunc');
+                expect(window.require).toHaveBeenCalled();
+                expect(window.require.argsForCall[0][0]).toEqual(['view.main']);
+                expect(typeof window.require.argsForCall[0][1]).toEqual('function');
+                requreFunction(testFunc);
+                expect(testFunc).toHaveBeenCalled();
+            })
+        });
+        describe('appView', function () {
+            it('should call require with view.app and a function', function () {
+                spyOn(window, 'require');
+                routerFunctions.appView('test');
+                var requreFunction = window.require.argsForCall[0][1];
+                var testFunc = jasmine.createSpy('testFunc');
+                expect(window.require).toHaveBeenCalled();
+                expect(window.require.argsForCall[0][0]).toEqual(['view.app']);
+                expect(typeof window.require.argsForCall[0][1]).toEqual('function');
+                requreFunction(testFunc);
+                expect(testFunc).toHaveBeenCalledWith({id:'test'});
+            })
+        });
+        describe('customShortcutView', function () {
+            it('should call require with view.app and a function', function () {
+                spyOn(window, 'require');
+                routerFunctions.customShortcutView();
+                var requreFunction = window.require.argsForCall[0][1];
+                var testFunc = jasmine.createSpy('testFunc');
+                expect(window.require).toHaveBeenCalled();
+                expect(window.require.argsForCall[0][0]).toEqual(['view.custom.shortcut']);
+                expect(typeof window.require.argsForCall[0][1]).toEqual('function');
+                requreFunction(testFunc);
+                expect(testFunc).toHaveBeenCalled();
+            })
+        });
 
     });
 
