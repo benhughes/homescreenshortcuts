0f0dd8a3ffa2b9d8678ccaa8e47f1937b0eb9c05
Ben Hughes <benhuggy@gmail.com>
2013-08-18 15:28:44 +0100

    isURI made more bulletproof

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 define([], function () {
     return {
         isURI: function (URI) {
-            return URI.match('://') !== null
+            return typeof URI == 'string' && URI.match('://') !== null
         },
         navigateTo: function (url) {
             location.href = url;
