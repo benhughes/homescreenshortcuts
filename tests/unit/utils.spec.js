562b0658f5549b33c76a896e2428dff3c52bc496
Ben Hughes <benhuggy@gmail.com>
2013-08-18 15:01:30 +0100

    First unit tests and karma set up config

---
+++ tests/unit/utils.spec.js
@@ -0,0 +1,9 @@
+define(['utils'], function (utils) {
+
+    describe('utils', function () {
+        it('should be exist', function () {
+            expect(typeof utils).not.toEqual('undefined');
+        });
+    });
+
+});
