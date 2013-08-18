029f6b1865ccb9c50f7a1114b9c9104f516509d0
Ben Hughes <benhuggy@gmail.com>
2013-08-18 15:28:17 +0100

    utils test spec updated

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -4,6 +4,20 @@ define(['utils'], function (utils) {
         it('should be exist', function () {
             expect(typeof utils).not.toEqual('undefined');
         });
+        describe('isURI', function () {
+            it('should return true if passed param contains ://', function () {
+                expect(utils.isURI('test://test')).toEqual(true);
+            });
+            it('should return false if passed param does not contain :// or they are non strings', function () {
+                expect(utils.isURI('test:/test')).toEqual(false);
+                expect(utils.isURI('test//test')).toEqual(false);
+                expect(utils.isURI('')).toEqual(false);
+                expect(utils.isURI()).toEqual(false);
+                expect(utils.isURI(345)).toEqual(false);
+                expect(utils.isURI({})).toEqual(false);
+                expect(utils.isURI({})).toEqual(false);
+            });
+        })
     });
 
 });
